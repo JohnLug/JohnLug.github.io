de34f5433e7b5d23bb92e553f0d1d4c18a8cf7ee
JohnLugue <148310361+JohnLug@users.noreply.github.com>
2023-11-18 01:28:39 -0700

    progression

--- script.js
+++ script.js
@@ -1,30 +1,87 @@
-import Deck from "./deck.js"
+import Deck from "./deck.js";
 
-const roles = ["Dispatcher", "Medic",  "Quarantine", "Reasearcher", "Scientist"]
+const BLUES = ["San Francisco", "Chicago", "Atlanta", "Toronto", "New York", "Washington", "London", "Madrid", "Paris", "Milan", "Essen", "St Petersburg"];
+const REDS = ["Seoul", "Tokyo", "Osaka", "Beijing", "Shanghai", "Taipei", "Hong Kong", "Manila", "Bangkok", "Ho Chi Minh City", "Jakarta", "Sydney"];
+const BLACKS = ["Moscow", "Istanbul", "Baghdad", "Tehran", "Algiers", "Cairo", "Riyadh", "Karachi", "Delhi", "Kolkata", "Mumbai", "Chennai"]
+const YELLOWS = ["Los Angeles", "Mexico City", "Miami", "Sao Paulo", "Lagos", "Khartoum", "Santiago", "Lima", "Bogota", "Buenos Aires", "Kinshasa", "Johannesburg"];
+const roles = ["Dispatcher", "Medic",  "Quarantine", "Reasearcher", "Scientist"];
+const playerDeck = document.querySelector('.playerdeck');
+const map = document.querySelector('.map');
 
-const deckInfection = new Deck()
-const deckPlayer = new Deck()
-deckInfection.shuffleDeck()
-deckPlayer.shuffleDeck()
+let gameRunning = false;
+let infectedCity = '';
 
-var outbreakNum = 0;
+setUp();
 
-const playerOne = {
-    role: "Dispatcher",
-    hand: [],
-    actionPoints: 0
+function setUp() {
+    gameRunning = true
 
-};
+    const deckInfection = new Deck();
+    const deckPlayer = new Deck();
+    deckInfection.shuffleDeck();
+    deckPlayer.shuffleDeck();
 
-const playerTwo = {
-    role: "Scientist",
-    hand: [],
-    actionPoints: 0
+    var outbreakNum = 0;
 
-};
+    const playerOne = {
+        role: "Dispatcher",
+        hand: deckPlayer.cards.slice(0, 3),
+        actionPoints: 3
 
-console.log(deckInfection.cards)
-console.log(deckPlayer.cards) 
+    };
+
+    const playerTwo = {
+        role: "Scientist",
+        hand: deckPlayer.cards.slice(3, 6),
+        actionPoints: 3
+
+    };
+
+    // make map first 
+    
+    infectedCity = deckInfection.pop();
+    infectedCity();
+} 
+
+function infectCity() {
+
+}
+
+
+function makeMap() {
+    var maxRow = 5;
+    var maxColumn = 10;
+    var row = 1;
+    var column = 1;
+    BLUES.forEach(function(cityName) {
+        const city = document.createElement('div');
+        city.classList.add('blueCity');
+        if (row < maxRow){
+            if(column < maxColumn) {
+                city.dataset.value = `(${row} ${column})`
+                column++;
+            } else {
+                column = 1;
+            }
+        } else {
+            row = 1;
+        }
+
+        city.dataset.value = `(${})`
+        city.innerText = cityName;
+
+        map.appendChild(city);
+
+
+    });
+}
+
+function checkWin() {
+    
+}
+
+// console.log(deckInfection.cards)
+// console.log(deckPlayer.cards) 
 
 function setup () {}
 // stuff to get the game ready and started like setting infections on cities, 
